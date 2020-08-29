e82f28cb89d1860411c83340ec6741601531e5b1
PARKNAYEON <parkna96@gmail.com>
2020-08-29 17:08:39 +0900

    지도뷰

--- app/src/main/java/com/example/cozy/views/map/MapAdapter.kt
+++ app/src/main/java/com/example/cozy/views/map/MapAdapter.kt
@@ -2,7 +2,6 @@ package com.example.cozy.views.map
 
 import android.content.Context
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.cozy.R
@@ -11,7 +10,7 @@ import com.example.cozy.R
 class MapAdapter (private val context : Context, val itemClick: (MapData) -> Unit) : RecyclerView.Adapter<MapViewHolder>(){
     var data = mutableListOf<MapData>()
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MapViewHolder {
-        val view = LayoutInflater.from(context).inflate(R.layout.item_recommend,parent,false)
+        val view = LayoutInflater.from(context).inflate(R.layout.item_bookstore_list,parent,false)
         return MapViewHolder(view, itemClick)
     }
 
--- app/src/main/java/com/example/cozy/views/map/MapFragment.kt
+++ app/src/main/java/com/example/cozy/views/map/MapFragment.kt
@@ -3,6 +3,7 @@ package com.example.cozy.views.map
 import android.content.Intent
 import android.os.Bundle
 import android.view.*
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.cozy.ItemDecoration
 import com.example.cozy.MainActivity
@@ -40,7 +41,7 @@ class MapFragment : Fragment() {
 
     fun loadData(v : View){
         data.apply{
-            for ( i in 0..7) {
+
                 add(
                     MapData(
                         bookstoreIdx = 1,
@@ -52,11 +53,55 @@ class MapFragment : Fragment() {
                         tag3 = "베이커리"
                     )
                 )
-            }
+            add(
+                MapData(
+                    bookstoreIdx = 1,
+                    img = "https://cdn.pixabay.com/photo/2015/11/19/21/11/knowledge-1052014__340.jpg",
+                    bookstoreName = "코지책방",
+                    location = "서울특별시 용산구 한강대로102길 용산구 한강",
+                    tag1 = "베이커리",
+                    tag2 = "베이커리",
+                    tag3 = "베이커리"
+                )
+            )
+            add(
+                MapData(
+                    bookstoreIdx = 1,
+                    img = "https://cdn.pixabay.com/photo/2015/11/19/21/11/knowledge-1052014__340.jpg",
+                    bookstoreName = "코지책방",
+                    location = "서울특별시 용산구 한강대로102길 용산구 한강",
+                    tag1 = "베이커리",
+                    tag2 = "베이커리",
+                    tag3 = "베이커리"
+                )
+            )
+            add(
+                MapData(
+                    bookstoreIdx = 1,
+                    img = "https://cdn.pixabay.com/photo/2015/11/19/21/11/knowledge-1052014__340.jpg",
+                    bookstoreName = "코지책방",
+                    location = "서울특별시 용산구 한강대로102길 용산구 한강",
+                    tag1 = "베이커리",
+                    tag2 = "베이커리",
+                    tag3 = "베이커리"
+                )
+            )
+
         }
         mapAdapter.data = data
         v.rv_map.addItemDecoration(ItemDecoration(this.context!!, 0,36))
         mapAdapter.notifyDataSetChanged()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+//        inflater.inflate(R.menu.search, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.search -> Toast.makeText(context,"검색",Toast.LENGTH_SHORT).show()
+        }
+        return super.onOptionsItemSelected(item)
     }
 
 }
