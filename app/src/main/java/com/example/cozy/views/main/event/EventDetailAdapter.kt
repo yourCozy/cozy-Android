459e2a7c9aca58d3af826ba9c27678f32ac7d5d9
PARKNAYEON <parkna96@gmail.com>
2020-09-18 00:19:20 +0900

    [NY] 수정

--- app/src/main/java/com/example/cozy/views/main/event/EventDetailAdapter.kt
+++ app/src/main/java/com/example/cozy/views/main/event/EventDetailAdapter.kt
@@ -6,11 +6,8 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.cozy.R
-import com.example.cozy.views.main.event.comment.CommentData
-
 
 class EventDetailAdapter (private val context : Context, private var data : Array<String?>): RecyclerView.Adapter<EventDetailViewHolder>(){
-
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): EventDetailViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.item_event_items, parent, false)
@@ -28,28 +25,4 @@ class EventDetailAdapter (private val context : Context, private var data : Arra
 
     }
 
-    fun validImage(data: EventDetailData): Array<String?> {
-        var cnt : Int = 0
-        val arrayImage = arrayOf(
-            data.image1,
-            data.image2,
-            data.image3,
-            data.image4,
-            data.image5,
-            data.image6,
-            data.image7,
-            data.image8,
-            data.image9,
-            data.image10
-        )
-        for(i in 0..9){
-            if(arrayImage[i] == null){
-                cnt = i //까지만 valid한 데이터
-                break
-            }
-        }
-        val arrayValidImage = arrayImage.copyOfRange(0,cnt)
-
-        return arrayValidImage;
-    }
 }
--- app/src/main/java/com/example/cozy/views/main/event/EventViewHolder.kt
+++ app/src/main/java/com/example/cozy/views/main/event/EventViewHolder.kt
@@ -1,6 +1,5 @@
 package com.example.cozy.views.main.event
 
-
 import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
