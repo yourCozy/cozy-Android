27a375aec8be1f135b13498706e03196657b29bc
haileyHi <yuhiro@naver.com>
2020-09-19 16:06:23 +0900

    [Hi] open source license 액티비티 추가!

---
+++ app/src/main/java/com/example/cozy/views/mypage/setting/OpenSourceLicenseActivity.kt
@@ -0,0 +1,12 @@
+package com.example.cozy.views.mypage.setting
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.cozy.R
+
+class OpenSourceLicenseActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_open_source)
+    }
+}
